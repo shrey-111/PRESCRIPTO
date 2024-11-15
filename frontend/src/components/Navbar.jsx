11ad34556cc1fbf669e6c1e24ff862ae354423a1
SHREYA <123317004+shrey-111@users.noreply.github.com>
2024-11-15 19:31:06 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -36,6 +36,7 @@ const Navbar = () => {
           <li className='py-1'>CONTACT</li>
           <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
         </NavLink>
+        <a target="_blank" href="https://prescripto-admin-gzpl.onrender.com" class="border px-5 text-xs py-1.5 rounded-full">Admin Panel</a>
       </ul>
 
       <div className='flex items-center gap-4 '>
